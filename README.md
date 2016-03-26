0fbda33b681f498f519da44b14a73150fc170e91
p1nh0 <p1nh0.c0d1ng@gmail.com>
2016-03-26 20:12:03 +0000

    clean and merge from master
    RAWGIT wasn’t possibly the best solution

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Transarkiv
 Web-based audio archive mixer and recorder
 
-
-This github page is redirecting to the index.html file on the master-branch using RAWGIT
-
-
 # Preview Link
 http://sonoscopia.github.io/Transarkiv/ 
---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+var filenames = [];
+var player1;
+var som;
+var path = 'sounds/';
+
+function preload(){
+  fileNames(); // array filenames[] (precisa de ser iniciado em preload)
+}
+
+function setup() {
+  createCanvas(800, 400);
+  
+  player1 = new Player(150, 150); //novo player
+  player2 = new Player(350, 170); //novo player
+  //print(path);
+  
+}
+
+function draw() {
+  background(55);
+
+  player1.display();
+  player2.display();
+  
+}
+
+function mousePressed(){
+  player1.clicked();
+  player2.clicked();
+}
