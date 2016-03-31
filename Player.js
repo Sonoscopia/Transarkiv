0c535d5add440858f7bfb07c2885dbed05b0ea70
rdias74 <rui@rdmbp11.lan>
2016-03-31 11:29:46 +0100

    vários: this.move(), this.autoplay()
    - move e autoplay: os players movem-se e escolhem sons automaticamente.
    
    - classes Fader e Toggle (ainda não estão ativadas);

--- Player.js
+++ Player.js
@@ -3,55 +3,111 @@
 function Player(x_, y_) {
   this.x = x_;
   this.y = y_;
-  this.size = 80;
-  this.playing = false; //
+  this.size = 50;
+  this.speed = [random(-0.2, 0.2), random(-0.2, 0.2)];
+  this.time = 0;
+  this.lapse = 50; //5 seconds
+  this.autoPlay_toggle = false;
+  this.mouseLock = false;
+  this.hover = false;
 
-  this.color_stopped = color(200, 199, 199, 111);
+  this.color_stopped = color(200, 111);
+  this.color_hover = color(222, 222);
   this.color_loading = color(33, 33, 33, 222);
-  this.color_playing = color(233, 233, 233, 222);
+  this.color_playing = color(233, 133);
+  this.color_transport = color(255, 155);
+  //botão Play
+  this.playToggle = new Toggle(this.x - this.size / 2, this.y + this.size / 2, 15);
+  this.playToggle.setMode('CIRC');
   //botão next random file
-  this.next_x = this.x + this.size / 2;
-  this.next_y = this.y + this.size / 2;
-  this.next_size = 20;
+  this.next = new Toggle(this.x + this.size / 2, this.y - this.size / 2, 15);
+  this.next.setMode('CIRC');
+
   //sinal loading
   this.loading_x = this.x - this.size / 2;
   this.loading_y = this.y - this.size / 2;
-  
 
-  this.fileNumber = 0;
+  this.fileNumber = int(random(3));
   this.fileName = filenames[this.fileNumber];
-  //carregar o som - -  aqui devia dar para utilizar uma função callback para mostrar quando está a carregar
 
+  //carregar o som - -  aqui devia dar para utilizar uma função callback para mostrar quando está a carregar
   this.sound = loadSound(path + filenames[this.fileNumber]);
+  if (this.sound.isLoaded()) {
+    print('sound is Loaded!!!!!!');
+  }
   this.amp = new p5.Amplitude();
   this.amp.setInput(this.sound);
   this.level = 0;
   this.loading_x = this.x - this.size / 2;
   this.loading_y = this.y - this.size / 2;
+  this.currentTime = 0;
+  this.ct = 0;
   
-  println('Player ready');
+  this.playFor = int(random(10, this.sound.duration() * 10));
+  //int(random(10, this.sound.duration()) * 0.01); // With Autoplay, play this file for how long (milliseconds)
 
   this.display = function() {
-    //botão next random file
-    fill(222, 222, 222);
-    ellipse(this.x + this.size / 2, this.y + this.size / 2, this.next_size, this.next_size);
+    this.time = int(millis() * 0.01);
+    if (this.time > 30) {
+      this.autoPlay_toggle = true
+    }
+    //print(this.time);
+    //Autoplay
+    //this.autoPlay();
 
+    if (this.mouseLock) {
+      this.x = mouseX;
+      this.y = mouseY;
+    }
+    //detect mouse hover
+    this.h_dist = int(dist(this.x, this.y, mouseX, mouseY));
+    if (this.h_dist < this.size / 2) {
+      this.hover = true;
+    } else {
+      this.hover = false;
+    }
+
+    //Amplitude
+    this.level = this.amp.getLevel();
+    this.level = this.level * 50;
     //botão principal
-    if (this.sound.isPlaying()){//this.playing) {
+    if (this.sound.isPlaying()) { //this.playing) {
       fill(this.color_playing);
     } else {
       fill(this.color_stopped);
     }
-    //Amplitude
-    this.level = this.amp.getLevel();
-    this.level = this.level * 50;
+    if (this.hover) {
+      fill(this.color_hover);
+    }
+
+    noStroke();
     ellipse(this.x, this.y, this.size + this.level, this.size + this.level);
 
+    //botão Play
+    this.playToggle.setPos(this.x - this.size / 2, this.y + this.size / 2);
+    this.playToggle.display();
     //nome do ficheiro
     fill(166, 166, 166);
-    text(this.fileName, this.x + this.size / 2 + 15, this.y + this.size / 2 + 5);
-    //print('-- isLoaded: '+ this.sound.isLoaded + ' - - - ');
-    
+    textAlign('LEFT', 'CENTER');
+    text(this.fileName, this.x - this.size / 2 + 25, this.y + this.size / 2 + 10);
+
+    //botão next random file
+    this.next.setPos(this.x + this.size / 2, this.y - this.size / 2);
+    this.next.display();
+
+
+    //Barra de transporte
+    this.arcSize = this.size;
+    this.rad = radians(90);
+    strokeWeight(1);
+    noFill();
+    stroke(55, 111);
+    arc(this.x, this.y, this.arcSize, this.arcSize, this.rad, this.rad + radians(359));
+    strokeWeight(5);
+    this.currentTime = this.sound.currentTime();
+    this.ct = map(this.currentTime, 0, this.sound.duration(), 0, radians(359));
+    stroke(this.color_transport);
+    arc(this.x, this.y, this.arcSize, this.arcSize, this.rad, this.rad + this.ct);
 
     //sinal loading
     if (this.sound.isLoaded()) {
@@ -62,13 +118,66 @@ function Player(x_, y_) {
       ellipse(this.x, this.y, this.size, this.size);
     }
 
+    //Texto informativo
+    if (this.hover) {
+      text(filenames[this.fileNumber], 40, height - 100);
+    }
+
+
   }
 
+  this.move = function() {
+    this.x += this.speed[0];
+    if (this.x > width - 50 || this.x < 50) {
+      this.speed[0] *= -1;
+    }
+    this.y += this.speed[1];
+    if (this.y > height - 50 || this.y < 50) {
+      this.speed[1] *= -1;
+    }
+    this.t = this.time % this.lapse;
+    //print('t: ' +this.t + '  -  time: ' +this.time + '  -  lapse: ' + this.lapse);
+
+    if (this.t > this.lapse - 5) {
+      this.speed[0] += random(-0.1, 0.1);
+      if (abs(this.speed[0]) > 0.3) {
+        this.speed[0] = random(-0.1, 0.1);
+      }
+      this.speed[1] += random(-0.1, 0.1);
+      if (abs(this.speed[1]) > 0.3) {
+        this.speed[1] = random(-0.1, 0.1);
+      }
+      this.lapse = int(random(15)) + 15;
+    }
+
+  }
+
+  this.autoPlay = function() {
+    if (this.autoPlay_toggle) {
+      //print(this.sound.duration());
+      this.time_modulo = int(this.time % this.playFor);
+      //print('time: ' + this.time + '  - time_modulo: ' + this.time_modulo + '  -  playFor: ' + this.playFor);
+
+      if (this.time_modulo > this.playFor - 2) {
+        if (this.sound.isLoaded()) {
+          if (this.sound.isPlaying()) {
+            this.sound.stop();
+            this.selectRandom();
+          } else {
+            this.sound.play();
+          }
+          this.playFor = int(random(10, this.sound.duration()*10));
+        }
+      }
+    }
+  }
 
   this.clicked = function() {
     //detetar clique no botão principal
     var d = int(dist(this.x, this.y, mouseX, mouseY));
-
+    if (d < this.size / 2) {
+      this.mouseLock = true;
+    }
     if (d < this.size / 2 && this.sound.isLoaded()) {
       if (this.sound.isPlaying()) {
         this.sound.stop();
@@ -76,25 +185,27 @@ function Player(x_, y_) {
         this.sound.play();
       }
     }
+    //botão Play
+    this.playToggle.clicked();
 
     //detetar clique no botão next random
+    this.next.clicked();
     d = int(dist(this.next_x, this.next_y, mouseX, mouseY));
     if (d < this.next_size) {
       this.selectRandom();
     }
-
+  }
+  this.released = function() {
+    this.mouseLock = false;
   }
 
   this.selectRandom = function() {
     if (this.sound.isPlaying()) {
       this.sound.stop();
     }
-
     this.fileNumber = int(random(filenames.length));
     this.fileName = filenames[this.fileNumber];
-
     this.sound = loadSound(path + filenames[this.fileNumber]);
-
     this.amp.setInput(this.sound);
     print(this.fileNumber + ': ' + filenames[this.fileNumber]);
   }
--- sketch.js
+++ sketch.js
@@ -1,48 +1,68 @@
 var filenames = [];
+var files_count;
+
 var path = 'sounds/';
-var player1, player2;
+
+var player_count = 5;
+var players = []; //array de objetos da classe 'Player'
 var vol = 1;
 var masterFader;
 var waveform, spectrum, fft;
 var fft;
-
+var tog;
 
 function preload() {
   fileNames(); // array filenames[] (precisa de ser iniciado em preload)
 }
 
 function setup() {
-  var canvas = createCanvas(900, 400);
+  var canvas = createCanvas(800, 500);
   canvas.parent("p5canvas");
 
-  player1 = new Player(200, 150); //novo player
-  player2 = new Player(550, 170); //novo player
-  masterFader = new Fader(700, height-110, 0.8);
   fft = new p5.FFT();
+  //criar os players
+  for (var i = 0; i < player_count; i++) {
+    players[i] = new Player(i*(width/player_count)+50, random(height-200)+100); //novo player
+  }
 
+  masterFader = new Fader(width - 60, height - 130, 30, 120, 0.8); //controlo de volume geral
+
+  tog = new Toggle(200, 200);
 }
 
 function draw() {
-  background(55);
+  background(33);
   noStroke();
-  player1.display();
-  player2.display();
+
+  for (var i = 0; i < player_count; i++) {
+    players[i].display();
+    players[i].move();
+    players[i].autoPlay();
+  }
   displaySpectrum();
   displayWave();
 
   vol = masterFader.getValue();
   masterVolume(vol);
   masterFader.display();
-
+  tog.display();
+  
+  
 }
 
 function mousePressed() {
-  player1.clicked();
-  player2.clicked();
+  for (var i = 0; i < player_count; i++) {
+    players[i].clicked();
+  }
+
   masterFader.clicked();
 }
-function mouseReleased(){
+
+function mouseReleased() {
   masterFader.released();
+  for (var i = 0; i < player_count; i++) {
+    players[i].released();
+  }
 }
 
 function detectMouse(_x, _y, _w, _h) {
@@ -77,11 +97,12 @@ function displaySpectrum() {
   var spectrum_init_x = width / 2 - spectrum_size[0] / 2;
   var spectrum_init_y = height - 100;
 
-  for (var i = 0; i < spectrum.length/2; i++) {
-    var spectrum_x = map(i, 0, spectrum.length/2, 0, spectrum_size[0]);
+  for (var i = 0; i < spectrum.length / 2; i++) {
+    var spectrum_x = map(i, 0, spectrum.length / 2, 0, spectrum_size[0]);
     spectrum_x += spectrum_init_x;
     var spectrum_h = map(-spectrum[i], 0, 255, 0, spectrum_size[1]);
     spectrum_h += spectrum_init_y + spectrum_size[1];
+    strokeWeight(1);
     stroke(spectrum[i]);
     line(spectrum_x, spectrum_init_y + spectrum_size[1], spectrum_x, spectrum_h);
   }
