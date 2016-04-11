d7631f3dfd7306ca2180bf5b65dfcd249d8f89ee
p1nh0 <p1nh0.c0d1ng@gmail.com>
2016-04-11 19:49:52 +0100

    Added iconOff and iconOn

--- cHandler.js
+++ cHandler.js
@@ -5,8 +5,8 @@ function cHandler(x_, y_, s_) {
   this.x = x_;
   this.y = y_;
   this.size = s_;
-  this.value_x = 0;
-  this.value_y = 0;
+  this.value_x = 0; // mouse UI
+  this.value_y = 0; // mouse UI
 
   this.color_on = color(255, 211);
   this.color_off = color(255, 111);
@@ -16,6 +16,14 @@ function cHandler(x_, y_, s_) {
   this.icon;
   this.icon_x = 0; //icon position offset
   this.icon_y = 0; 
+  this.iconOn, this.iconOff;
+  // icon On offset
+  this.iconOn_x = 0;
+  this.iconOn_y = 0;
+  // icon Off offset
+  this.iconOff_x = 0;
+  this.iconOff_y = 0;
+  this.toggle = false; // toggle on off value
   
   this.mouseLock = false;
   this.lastMouseY = 0;
@@ -41,6 +49,12 @@ function cHandler(x_, y_, s_) {
     ellipse(this.x, this.y, this.size, this.size);
     if (this.icon){ // draw button icon
       image(this.icon, this.x+this.icon_x, this.y+this.icon_y);
+    }
+    if(this.iconOn && this.iconOff){
+      if(this.toggle)
+        image(this.iconOn, this.x+this.iconOn_x, this.y+this.iconOn_y);
+      else
+        image(this.iconOff, this.x+this.iconOff_x, this.y+this.iconOff_y);
     }
     
     //set value
@@ -75,6 +89,7 @@ function cHandler(x_, y_, s_) {
       this.mouseLock = true;
       this.lastMouseX = mouseX;
       this.lastMouseY = mouseY;
+      this.toggle = !this.toggle;
     }
   }
   this.released = function() {
@@ -140,11 +155,25 @@ function cHandler(x_, y_, s_) {
   this.setRange = function(r_) {
     this.range = radians(r_);
   }
-  this.setIcon = function(file_){ // set image at this.x & this.y
+  this.setIcon = function(file_){ // set single icon
     this.icon = loadImage(file_);
   }
   this.setIconOffset = function(x_, y_){ // set image plus XY offset
     this.icon_x = x_;
     this.icon_y = y_;
+  }
+   this.setIconOn = function(on_){ // set icon On
+    this.iconOn = loadImage(on_);
+  }
+  this.setIconOff = function(off_){
+    this.iconOff = loadImage(off_);
+  }
+  this.setIconOnOffset = function(x_, y_){
+    this.iconOn_x = x_;
+    this.iconOn_y = y_;
+  }
+  this.setIconOffOffset = function(x_, y_){
+    this.iconOff_x = x_;
+    this.iconOff_y = y_;
   }
 }
