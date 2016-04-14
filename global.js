5732e7d427444f5aa75d0c420c6dadcd127e9834
p1nh0 <p1nh0.c0d1ng@gmail.com>
2016-04-15 00:28:11 +0100

    updated sound paths

--- global.js
+++ global.js
@@ -189,33 +189,44 @@ function displayWave() {
 
 /****************** SOUND FILES *****************************/
 function fileNames() {
-  category_path[0] = '01_Ambient/';
-  category_path[1] = '02_Rhythmic/';
-  category_path[2] = '03_Detail/';
-  category_path[3] = '04_Voice/';
+  category_path[0] = '01_Field Recordings/';
+  category_path[1] = '02_Ambient/';
+  category_path[2] = '03_Textural/';
+  category_path[3] = '04_Rhythmic/';
+  category_path[4] = '05_Melodic/';
+  category_path[5] = '06_Performance/';
   
   //instantiate nested arrays
   filenames[0] = [];
   filenames[1] = [];
   filenames[2] = [];
-  filenames[3] = []; 
+  filenames[3] = [];
+  filenames[4] = [];
+  filenames[5] = [];
   
-  // Category #1 - Ambient
-  filenames[0][0] = '0001_MagliocchiFilipeDuo.mp3';
-  filenames[0][1] = '0002_RM_MV336.mp3';
-  filenames[0][2] = '0003_CostaGuimaraesViegas.mp3';
+  // Category #1 - Field Recordings
+  filenames[0][0] = 'Amolador-de-facas-e-gaivota.mp3';
+  filenames[0][1] = 'Beinham-ajudar-uma-ceguinha.mp3';
 
-  // Category #2 - Rhythmic
-  filenames[1][0] = '0001_MV_3_34_GustavoC_HenriqueF_RAmado.mp3';
-  filenames[1][1] = '0002_MV334_Montagem_F.mp3';
+  // Category #2 - Ambient
+  filenames[1][0] = 'Classic-synth.mp3';
+  filenames[1][1] = 'Granular-springs.mp3';
   
-  // Category #3 - Detail
-  filenames[2][0] = '0001_AbdulRosso_CRA.mp3';
-  filenames[2][1] = '0002_TheoCeccaldi_MarceloReis_ValentinCeccaldi_LuisVicente_Improvisation1.mp3';
+  // Category #3 - Textural
+  filenames[2][0] = 'Bass-scratch.mp3';
+  filenames[2][1] = 'Col-legno.mp3';
   
-  // Category #4 - Voice
-  filenames[3][0] = '0001_Frogs.mp3';
-  filenames[3][1] = '0002_Frogs.mp3';
+  // Category #4 - Rhythmic
+  filenames[3][0] = 'Granular-machine.mp3';
+  filenames[3][1] = 'Low-beat.mp3';
+  
+  // Category #5 - Melodic
+  filenames[4][0] = 'Filtered-clicks.mp3'
+  filenames[4][1] = 'Flute-and-delay.mp3'
+  
+  // Category #6 - Performance
+  filenames[5][0] = '43-tone-metalophone.mp3';
+  filenames[5][1] = 'Acoustic-laptops.mp3';
   
   //print('filenames ready');
 }
